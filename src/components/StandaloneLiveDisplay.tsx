7dba36b5c57dfa583e890c9472adbc00e01c6477
Aric <aric01062008@gmail.com>
2026-05-21 09:58:41 +0530

    Delete src directory

--- src/components/StandaloneLiveDisplay.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { LiveDisplay } from './LiveDisplay';
-
-// This component is used for the standalone live display page
-export function StandaloneLiveDisplay() {
-  return <LiveDisplay teams={[]} isStandalone={true} />;
-}
